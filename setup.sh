71926a4c6029b06671e331693f5f138758e4e4d5
LauraLundbye <202106724@post.au.dk>
2024-09-25 11:32:18 +0200

    attempt 1

---
+++ setup.sh
@@ -0,0 +1,5 @@
+!/usr/bin/env bash
+
+git config --global user.name "LauraLundbye"
+git config --global user.email "202106724@post.au.dk" # adding git credentials 🙂
+# to run make sure you are in the correct folder in terminal and write bash setup.sh
